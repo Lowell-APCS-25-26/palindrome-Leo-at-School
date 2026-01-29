a5d010fcfa14310dce00cded84ad15da9f7bbffb
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2026-01-29 17:24:14 +0000

    Created first plaindrome and reverse functions

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,19 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        return reverse(s).equals(s);
     }
     public String reverse(String s) {
         String n = new String();
-        // your code here
+        
+        for (int i = s.length() - 1; i >= 0; i++){
+            if (i == s.length() - 1){
+                n += s.substring(i);
+            } else {
+                n += s.substring(i, i + 1);
+            }
+        }
+
         return n;
     }
 }
